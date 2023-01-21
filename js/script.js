b6e33846395bebcae9b02162cfa978e534f76e49
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-01-21 11:48:50 +0000

    created saveBtn for description and parent ID's still not working variable but I got the reference from https://api.jquery.com/parent/

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
+// Getting the date today.
 var todayDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").html(todayDate);
+
+// Reminder Referrence: https://learn.jquery.com/using-jquery-core/document-ready/
+$(document).ready(function(){
+  // Manipulate save button by calling class .saveBtn click listener
+  // Adding inside function for saveBtn and description values.
+  $(".saveBtn").on("click", function(){
+    // Reference: https://api.jquery.com/siblings/ for getting text of the .description (remember)
+    var text = $(this).siblings(".description").val();
+    // console.log($(this).siblings(".description").val());
+    var time = $(this).parent().attr("id");
+    // console.log($(this).parent());
+  })
+
+  function trackSchedule(){
+  // Get the time now.
+  // 
+    var currentTime = moment().hour();
+    // console.log(moment().hour()); (Working)
+  }
+})
+
+
+
+
+
+
