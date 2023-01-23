df79cd924ccf44cc2dbb59f3b512a5c7ce467b51
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-01-23 21:41:00 +0000

    all functional and working, however, I wanted to put delete or update button but the design of the icons where ruin the saveBtn tab. This will be in the next development of this app.

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ $(window).on("load", function(){
     // console.log($(this).siblings(".description").val());
     var time = $(this).parent().attr("id");
     // console.log($(this).parent());
+
+    // Saving the var text to localStorage.
+    localStorage.setItem(time, text);
   })
 
   // Load first.
@@ -57,6 +60,26 @@ $(window).on("load", function(){
       }
     })
   }
+  // Saving/Getting the items from local storage
+  $("#time8 .description").val(localStorage.getItem("time8"));
+  $("#time9 .description").val(localStorage.getItem("time9"));
+  $("#time10 .description").val(localStorage.getItem("time10"));
+  $("#time11 .description").val(localStorage.getItem("time11"));
+  $("#time12 .description").val(localStorage.getItem("time12"));
+  $("#time13 .description").val(localStorage.getItem("time13"));
+  $("#time14 .description").val(localStorage.getItem("time14"));
+  $("#time15 .description").val(localStorage.getItem("time15"));
+  $("#time16 .description").val(localStorage.getItem("time16"));
+  $("#time17 .description").val(localStorage.getItem("time17"));
+  $("#time18 .description").val(localStorage.getItem("time18"));
+  $("#time19 .description").val(localStorage.getItem("time19"));
+  $("#time20 .description").val(localStorage.getItem("time20"));
+
+  //
   trackSchedule();
 })
+
+// $(function() {
+//   $("#sortable").sortable();
+// } );
 
